d36ebc50bed2b0b43a0dbff24c4473935eb7ef79
jisu choi <pblwork1@gmail.com>
2016-09-28 14:08:45 +0900

    digital processing class practice project

---
+++ DigitalImageProcessing/ImageProcessing/ImageProcessing/resource.h
@@ -0,0 +1,32 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// ImageProcessing.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_ImageProcessingTYPE         130
+#define ID_WINDOW_MANAGER               131
+#define IDD_DIALOG1                     310
+#define IDD_DIALOG2                     311
+#define IDD_DIALOG3                     311
+#define IDD_DIALOG4                     315
+#define IDC_EDIT1                       1000
+#define ID_32771                        32771
+#define ID_DOWN_SAMPLING                32772
+#define ID_32773                        32773
+#define ID_QANTIZATION                  32774
+#define ID_QUANTIZATION                 32775
+#define ID_32776                        32776
+#define ID_UP_SAMPLING                  32777
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        315
+#define _APS_NEXT_COMMAND_VALUE         32778
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
